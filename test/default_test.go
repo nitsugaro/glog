42be0de35c1a7976edc7c19681d1360d5712ff47
nitsugaro <agusromero0815@gmail.com>
2025-04-15 20:54:56 -0300

    fix path

--- test/default_test.go
+++ test/default_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/natefinch/lumberjack"
-	glog "github.com/nitsugaro/glogger"
+	glog "github.com/nitsugaro/glog"
 	"github.com/sirupsen/logrus"
 )
 
